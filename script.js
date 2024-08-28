f0e81a6f3bb3679acbd00e58c55b212d40680464
Yuki70042 <82507110+Yuki70042@users.noreply.github.com>
2024-08-28 11:29:56 +0200

    Add "Enfance" Text

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ function showInfo(point) {
     let aboutText = document.getElementById('about-text');
     switch (point) {
         case 'Enfance':
-            aboutText.textContent = "Texte sur l'enfance.";
+            aboutText.innerHTML = "Né en Avril 2005, j'ai toujours été une enfant très curieuse adorant apprendre et créer des histoires.<br><br> Dès mon plus jeune âge je me suis tant retrouvé bercé dans l'art du jeux vidéos que cela est devenu une passion pour moi. J'y ai découvert des concepts, des univers, des histoires, des personnages aux caractères distingués que j'adorais redessiner et réinventer. <br> <br> Arrivée au collège les Mathématiques et l'Algorithmie sont des domaines qui m'ont particulièrement accrochés. J'ai eu l'occasion de participer à différents concours tel que \"Castor Informatique\" ou \"Algoréa\".";
             break;
         case 'Lycee':
-            aboutText.textContent = "Texte sur les années au lycée.";
+            aboutText.innerHTML = "Texte sur les années au lycée.";
             break;
         case 'BTS':
-            aboutText.textContent = "Texte sur les études en BTS SIO.";
+            aboutText.innerHTML = "Texte sur les études en BTS SIO.";
             break;
         case 'Aujourdhui':
-            aboutText.textContent = "Texte sur la situation actuelle.";
+            aboutText.innerHTML = "Texte sur la situation actuelle.";
             break;
         default:
-            aboutText.textContent = "Cliquez sur un point pour en savoir plus.";
+            aboutText.innerHTML = "Cliquez sur un point pour en savoir plus.";
     }
 
     // Déplace le curseur
