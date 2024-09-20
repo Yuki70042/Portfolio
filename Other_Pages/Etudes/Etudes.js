5a01298f86d42ba6005ce003b6c4a1ec8a039a08
Yuki70042 <lizlink2004@gmail.com>
2024-09-20 20:28:34 +0200

    Change Etude page

--- Other_Pages/Etudes/Etudes.js
+++ Other_Pages/Etudes/Etudes.js
@@ -1,7 +1,7 @@
 
 
 // Ajoute un menu "Burger sur les petits formats"
-document.getElementById('menu-toggle').onclick = function() {
+document.getElementById('menu-toggle').onclick = function () {
     var menu = document.getElementById('menu');
     if (menu.classList.contains('active')) {
         menu.classList.remove('active');
@@ -11,8 +11,17 @@ document.getElementById('menu-toggle').onclick = function() {
 };
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("year").textContent = new Date().getFullYear(); 
-    });
-    
-    
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("year").textContent = new Date().getFullYear();
+});
+
+
+
+function toggleSection(id) {
+    const section = document.getElementById(id);
+    if (section.classList.contains('active')) {
+        section.classList.remove('active');
+    } else {
+        section.classList.add('active');
+    }
+}
